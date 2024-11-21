e19c4888871844ad73d0cef70adae042d32671f8
Arturo Zepeda <aaze92@gmail.com>
2024-11-21 00:00:17 -0600

    añado funciones a BBDD

--- db/database.go
+++ db/database.go
@@ -5,7 +5,15 @@ import (
 	"log"
 )
 
-const file string = "Gasto.db"
+const (
+	file   string = "Gasto.db"
+	create string = `CREATE TABLE Gasto(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  descripcion TEXT NOT NULL,
+  monto REAL NOT NULL,
+  fecha TEXT NOT NULL,
+  categoria TEXT NOT NULL);`
+)
 
 func Conectar() (*sql.DB, error) {
 	db, err := sql.Open("sqlite3", file)
@@ -18,3 +26,20 @@ func Conectar() (*sql.DB, error) {
 	log.Println("Conexion exitosa a la base de datos: ", file)
 	return db, nil
 }
+
+func NuevosGastos() (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", file)
+	if err != nil {
+		return nil, err
+	}
+	_, err = db.Exec(create)
+	if err != nil {
+		return nil, err
+	}
+	log.Println("Base de datos creada exitosamente.")
+	return db, nil
+}
+
+/*func GetGastos (*sql.DB)(error){
+  res, err :=
+}*/
