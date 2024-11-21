111ab9d71035b453d06f5030fe4d2f58f7c0d481
Arturo Zepeda <aaze92@gmail.com>
2024-11-21 00:17:18 -0600

    añado funcion getGastos a BBDD

--- db/database.go
+++ db/database.go
@@ -5,6 +5,10 @@ import (
 	"log"
 )
 
+type GastoDB struct {
+	DB *sql.DB
+}
+
 const (
 	file   string = "Gasto.db"
 	create string = `CREATE TABLE Gasto(
@@ -27,19 +31,42 @@ func Conectar() (*sql.DB, error) {
 	return db, nil
 }
 
-func NuevosGastos() (*sql.DB, error) {
+func NewGastos() (*GastoDB, error) {
 	db, err := sql.Open("sqlite3", file)
 	if err != nil {
 		return nil, err
 	}
 	_, err = db.Exec(create)
 	if err != nil {
+		db.Close()
 		return nil, err
 	}
+
 	log.Println("Base de datos creada exitosamente.")
-	return db, nil
+	return &GastoDB{DB: db}, nil
 }
 
-/*func GetGastos (*sql.DB)(error){
-  res, err :=
-}*/
+func (g *GastoDB) GetGastos() ([]map[string]interface{}, error) {
+	rows, err := g.DB.Query(`SELECT * FROM Gasto`)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var gastos []map[string]interface{}
+	for rows.Next() {
+		var id int
+		var descripcion, fecha, categoria string
+		var monto float64
+		if err := rows.Scan(&id, &descripcion, &monto, &fecha, &categoria); err != nil {
+			return nil, err
+		}
+		gastos = append(gastos, map[string]interface{}{
+			"id":          id,
+			"descripcion": descripcion,
+			"monto":       monto,
+			"fecha":       fecha,
+			"categoria":   categoria,
+		})
+	}
+	return gastos, nil
+}
